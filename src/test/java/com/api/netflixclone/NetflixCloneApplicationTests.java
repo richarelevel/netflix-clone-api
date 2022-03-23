9de130b16888ed11be916588d139f4ef656ceead
richasinhaa <richa.sinha0603@gmail.com>
2022-03-23 16:33:47 +0800

    Netflix Clone API Template added

---
+++ src/test/java/com/api/netflixclone/NetflixCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.netflixclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
